3e5b1bd7e5e6687af7f29586c619a00039662cfe
raianymussak <raiany.mussak@escola.pr.gov.br>
2025-09-16 09:11:05 -0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-
 const botaoMostraPalavras = document.querySelector( "#botao-paçavrachave"); 
 
 botaoMostraPalavras.addEventListener( " click", mostraPalavrasChave); 
 
 function mostraPalavrasChave(){ 
     const texto = document.querySelector( "#entrada-de-texto").ariaValueMax;
-    
     const campoResultado = document. querySelector( "#resultado-palavrachave"); 
-     
-    const palavras = texto.split(""); 
+    const palavraChave = processaTexto(texto); 
+    
     campoResultado.textoContent = palavras; 
 
 }
