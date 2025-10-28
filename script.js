872ff9e6c15c49809a117a7e5362fdd9fed9a763
raianymussak <raiany.mussak@escola.pr.gov.br>
2025-10-28 09:06:05 -0300

    .

--- script.js
+++ script.js
@@ -12,26 +12,44 @@ function mostraPalavrasChave(){
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
-    const frequencias = contaFrequencias(palavras);
-    let ordenadas = Object.keys(frequencias).sort(ordenaPalavras);
 
-    function ordenaPalavras(p1, p2){
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
         return frequencias[p2] - frequencias[p1];
     }
 
     console.log(ordenadas);
     return ordenadas.slice(0,10);
 }
+
 function contaFrequencias(palavras){
     let frequencias = {};
     for (let i of palavras) {
         frequencias [i] =0; 
-    for(let j of palavras){
-        if(i==j) {
+        for(let j of palavras){
+            if(i==j) {
             frequencias[i]++;
-        }
+            }
         }
     }
     return palavras;
 }
 
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
+}
